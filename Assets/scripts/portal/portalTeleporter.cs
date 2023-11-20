9ce76ef40cc869a1c58e226f04e3591c81e19a46
Stefaaan <ste321fan@gmail.com>
2023-11-20 19:25:20 +0100

    changes

--- Assets/scripts/portal/portalTeleporter.cs
+++ Assets/scripts/portal/portalTeleporter.cs
@@ -1,31 +1,65 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class portalTeleporter : MonoBehaviour
 {
-    public Transform player;
-    public Transform reciever;
+    public Transform recieverTransform;
+    public Collider backCol;
+    public Collider otherCol;
+    
+    private Transform _playerTransform;
+    private Rigidbody _playerRigidbody;
+    private bool _playerIsOverlapping = false;
 
-    private bool playerIsOverlapping = false;
+    private int _cooldown = 160;
+    private int _currentCooldown = 0;
+    private bool _teleported;
 
-    void LateUpdate () {
-        if (playerIsOverlapping)
+    void Start()
+    {
+        _playerTransform = FindObjectOfType<PlayerMovement>().transform;
+        _playerRigidbody = FindObjectOfType<PlayerMovement>().GetComponent<Rigidbody>();
+    }
+
+    void Update () {
+        //cooldown
+        if(_teleported)
         {
-            Debug.Log("overlapping");
-            Vector3 portalToPlayer = player.position - transform.position;
-            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+            _currentCooldown++;
+            if (_currentCooldown >= _cooldown)
+            {
+                _teleported = false;
+                _currentCooldown = 0;
+                otherCol.enabled = true;
+            }
+        }
+        if (_playerIsOverlapping && !_teleported)
+        {
+            backCol.enabled = false;
+            Vector3 portalToPlayer = _playerTransform.position - transform.position;
+            float dotProduct = Vector3.Dot(transform.forward, portalToPlayer);
 
             if (dotProduct < 0f)
             {
-                float rotationDiff = -Quaternion.Angle(transform.rotation, reciever.rotation);
-                player.Rotate(Vector3.up, rotationDiff);
-
+                float rotationDiff = -Quaternion.Angle(transform.rotation, recieverTransform.rotation);
+                _playerTransform.Rotate(Vector3.up, rotationDiff);
+                
+                
                 Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
-                player.position = reciever.position + positionOffset;
+                _playerTransform.position = recieverTransform.position + positionOffset;
 
-                playerIsOverlapping = false;
+                _playerIsOverlapping = false;
+                _teleported = true;
+                otherCol.enabled = false;
+                _playerRigidbody.AddForce(0, 0, -transform.forward.z * 1000);
             }
+        }
+        else
+        {
+            backCol.enabled = true;
         }
     }
 
@@ -33,7 +67,7 @@ public class portalTeleporter : MonoBehaviour
     {
         if (other.CompareTag("player"))
         {
-            playerIsOverlapping = true;
+            _playerIsOverlapping = true;
         }
     }
     
@@ -41,7 +75,7 @@ public class portalTeleporter : MonoBehaviour
     {
         if (other.CompareTag("player"))
         {
-            playerIsOverlapping = true;
+            _playerIsOverlapping = true;
         }
     }
 
@@ -49,7 +83,7 @@ public class portalTeleporter : MonoBehaviour
     {
         if (other.CompareTag("player"))
         {
-            playerIsOverlapping = false;
+            _playerIsOverlapping = false;
         }
     }
 }
